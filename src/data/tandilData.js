ab0fb948a9b35b8da401bd888c49a8f0bee3ed1d
Ezequiel <EzequielWilsonBenito@gmail.com>
2026-08-04 22:56:07 -0300

    Cambios utiles

--- src/data/tandilData.js
+++ src/data/tandilData.js
@@ -19,5 +19,27 @@ export const lugaresTandil = [
     tipo: "Recreación",
     descripcion: "Espejo de agua artificial ideal para actividades náuticas y caminatas al aire libre.",
     imagen: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Lago_en_Tandil.jpg"
+  },
+  {
+    id: 4,
+    nombre: "Casa de Mer",
+    tipo: "Punto de venta",
+    descripcion: "Casa de Merlina, cerca de la facu.",
+    imagen: "https://planner5d.com/blog/content/images/2025/05/construir.casa.moderna.jpg"
+  },
+  {
+    id: 5,
+    nombre: "Carpincho reventado",
+    tipo: "Luto / Lúgubre",
+    descripcion: "Carpincho reventado.",
+    imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWY-Arb3pKkvqrHux58ixshty85DEPxU6kv_L20rBLxeehBOzhh9QuJBQ&s=10"
+  },
+  {
+    id: 6,
+    nombre: "Trampolín del bosque",
+    tipo: "Recreación",
+    descripcion: "Para saltar",
+    imagen: "https://images.bidcom.com.ar/resize?src=https://static.bidcom.com.ar/publicacionesML/productos/TRAMPFT8/1000x1000-TRAMPFT8-A.jpg&w=800&q=100"
   }
+
 ];
